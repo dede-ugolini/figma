7cfdc52ee5c7651bc5ee566bef6c4abf34b80b80
Andre Ugolini <andreugolini@unochapeco.edu.br>
2025-12-05 17:58:01 -0300

    add theme.

---
+++ src/themes/Theme.jsx
@@ -0,0 +1,51 @@
+import { createTheme } from "@mui/material";
+
+export const Theme = createTheme({
+  palette: {
+    primary: {
+      light: "#00B37E", // green-light
+      main: "#00875F", // green
+      dark: "#015F43", // green-dark
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#2D2D2E",
+      main: "#121214",
+      grayTwo: "#202024",
+      grayThree: "#29292E",
+      contrastText: "#fff",
+    },
+    background: {
+      body: "#202024",
+      header: "#141416"
+    },
+  },
+
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+    boxSpace: {
+      size: "30px",
+    },
+    card: {
+      size: "14px",
+      sizePrice: "24px",
+      sizeLine: "160%"
+    },
+    fontWeightRegular: 400,
+    fontWeightBold: 700,
+
+    body1: {
+      fontSize: '16px',
+      lineHeight: 1.6,
+    },
+
+    body2: {
+      fontSize: '14px',
+      lineHeight: 1.6,
+    },
+
+    h6: { fontSize: '20px', fontWeight: 700, lineHeight: 1.6 },
+    h5: { fontSize: '24px', fontWeight: 700, lineHeight: 1.6 },
+    h4: { fontSize: '32px', fontWeight: 700, lineHeight: 1.6 },
+  }
+});
