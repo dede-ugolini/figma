24faf012ad9cf33a883e4a3a153acc68155c59cf
Andre Ugolini <andreugolini@unochapeco.edu.br>
2025-12-08 14:26:13 -0300

    Adicionar ao componente Header a chamada de outros componentes.
    O componente Header vai estar chamando outro componente que retorna a
    logo e nome da empresa, e outro componente que retorna um botão para
    registrar uma nova transação.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,32 @@
 import { Theme } from "../themes/Theme";
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import NomeEmpresa from "./NomeEmpresa";
+import NewTransection from "./NewTransection";
 
 function Header() {
   return (
     <>
       <Box sx={{
-        width: '100%',
-        minHeight: '180px',
-        background: Theme.palette.background.header
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: Theme.palette.background.header,
       }}>
+        <Box sx={{
+          width: '80%',
+          minHeight: '180px',
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: 'center'
+        }}>
+          <Stack direction={"row"}>
+            <NomeEmpresa />
+          </Stack>
+          <Stack direction={"row"}>
+            <NewTransection />
+          </Stack>
+        </Box>
       </Box>
     </>
   )
