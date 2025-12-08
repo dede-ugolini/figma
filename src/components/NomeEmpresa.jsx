104072ce21eb538299635d8d5b3dec8e3ca9a90a
Andre Ugolini <andreugolini@unochapeco.edu.br>
2025-12-08 14:47:35 -0300

    Adicionar icone ao componente que retorna o nome da empresa.

--- src/components/NomeEmpresa.jsx
+++ src/components/NomeEmpresa.jsx
@@ -1,4 +1,14 @@
+import ParkIcon from '@mui/icons-material/Park';
+import { Theme } from '../themes/Theme';
+
 function NomeEmpresa() {
-  return <h1 style={{ fontSize: "20px", fontStyle: "normal" }}>Finance</h1>;
+  return (
+    <>
+      <ParkIcon sx={{
+        color: Theme.palette.primary.main
+      }}></ParkIcon>
+      <h1 style={{ fontSize: "20px", fontStyle: "normal" }}>Finance</h1>;
+    </>
+  )
 }
 export default NomeEmpresa;
