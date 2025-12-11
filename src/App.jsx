a49d9e422e2bb56483d17525867b79f50de886c8
Andre Ugolini <andreugolini@unochapeco.edu.br>
2025-12-11 13:41:35 -0300

    feat: filtragem funcionando.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
-import Name from './components/NomeEmpresa.jsx';
 import { Theme } from './themes/Theme.jsx';
-import { Container, Typography, Stack, Box } from "@mui/material";
+import { Container, Typography, Stack, Box, CardContent } from "@mui/material";
 import Saldo from "./components/Saldo.jsx";
 import Header from './components/Header.jsx';
-import SearchBar from './components/SearchBar.jsx';
+import TransactionContainer from './components/TransactionsContainer.jsx'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Container sx={{
+      <Stack sx={{ // Stack do Cabeçalho
+        height: "300px",
+        width: "100%"
+      }}>
+        <Header />
+      </Stack>
+
+      <Stack sx={{ // Stack que armazena os 3 cards de saldo e a o container que guarda a barra de pesquisa e transações.
         position: 'relative',
-        top: '-3rem',
+        top: '-10rem',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
       }}>
         <Saldo />
-      </Container>
-      <SearchBar />
+        <TransactionContainer />
+      </Stack>
 
       <Box sx={{
         backgroundColor: Theme.palette.background.body,
         width: '100%',
         height: '200vh'
-      }}></Box>
+      }}>
+
+      </Box>
+
 
     </>
   )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,8 @@ import { Stack, Button, Box, } from '@mui/material'
 import { Theme } from '../themes/Theme';
 import { MyTextField } from './MyTextField';
 import SearchIcon from '@mui/icons-material/Search';
-function SearchBar() {
+
+function SearchBar({ search, setSearch }) {
 
   return (
 
@@ -14,7 +15,11 @@ function SearchBar() {
         <Box sx={{
           width: "70%"
         }}>
-          <MyTextField label={"Busque uma Transação"} />
+          <MyTextField
+            label={"Busque uma Transação"}
+            inputValue={search}
+            setInputValue={setSearch}
+          />
         </Box>
         <Box sx={{
           width: "30%"
