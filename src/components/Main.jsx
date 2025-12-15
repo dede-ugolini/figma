ba1b41382330b1048b3d8cc5c448372770df7719
Andre Ugolini <andreugolini@unochapeco.edu.br>
2025-12-15 15:24:49 -0300

    :sparkless: feat: Adicionar componente Main que exibe componentes como o cabeçalho, barra de pesquisa, saldo, tabela de transações etc.

---
+++ src/components/Main.jsx
@@ -0,0 +1,51 @@
+import { Theme } from '../themes/Theme.jsx'
+import { Stack, Box } from '@mui/material'
+
+import Header from './Header.jsx'
+import Saldo from './Saldo.jsx'
+import TransactionContainer from './TransactionsContainer.jsx'
+
+export default function Main() {
+
+  return (
+    <>
+      <Stack sx={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: Theme.palette.primary.contrastText,
+      }}
+      >
+        <Stack sx={{ // Stack do Cabeçalho
+          height: "300px",
+          width: "100%"
+        }}>
+          <Header />
+        </Stack>
+
+        <Stack sx={{ // Stack que armazena os 3 cards de saldo e a o container que guarda a barra de pesquisa e transações.
+          position: 'relative',
+          top: '-10rem',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: '80%'
+        }}>
+          <Saldo />
+          <TransactionContainer />
+        </Stack>
+
+        <Box sx={{
+          backgroundColor: Theme.palette.background.body,
+          width: '100%',
+          height: '200vh'
+        }}>
+
+        </Box>
+
+      </Stack>
+    </>
+
+  )
+}
