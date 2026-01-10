edadc1f907fffbb6c94876afc989964cd3ae35e9
Andre Ugolini <andreugolini@unochapeco.edu.br>
2026-01-10 09:25:19 -0300

    style: Adicionar estilização a barra de pesquisa.

--- src/components/transactions/SearchBar.jsx
+++ src/components/transactions/SearchBar.jsx
@@ -1,6 +1,5 @@
-import { Stack, Button, Box, } from '@mui/material'
+import { Stack, Button, Box, TextField, } from '@mui/material'
 import { Theme } from '../../themes/Theme';
-import { MyTextField } from '../MyTextField';
 import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar({ search, setSearch }) {
@@ -8,17 +7,37 @@ function SearchBar({ search, setSearch }) {
   return (
 
     <>
-      <Stack direction='collumn' sx={{
+      <Stack spacing={2} direction='collumn' sx={{
         width: "100%",
         height: "40px"
       }}>
         <Stack sx={{
           width: "70%"
         }}>
-          <MyTextField
+          <TextField
             label={"Busque uma Transação"}
-            inputValue={search}
-            setInputValue={setSearch}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            sx={{
+              color: "secondary.main",
+              backgroundColor: Theme.palette.secondary.light,
+              borderRadius: "10px",
+              '& .MuiInputBase-input': { // Cor do texto de input do usuário
+                color: Theme.palette.primary.contrastText,
+              },
+              '& .MuiInputLabel-root': { // Cor do label do TextField
+                color: Theme.palette.primary.contrastText,
+              },
+              '& .MuiOutlinedInput-root': {
+                '&:hover fieldset': {
+                  borderColor: Theme.palette.primary.contrastText, // Cor do TextField ao passar o mouse
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: Theme.palette.secondary.grayThree, // Cor do TextField ao clicá-lo
+                },
+              },
+              input: { color: "" }
+            }}
           />
         </Stack>
         <Stack sx={{
@@ -36,10 +55,14 @@ function SearchBar({ search, setSearch }) {
               textTransform: "none",
               "&:hover": {
                 backgroundColor: Theme.palette.primary.light,
+                color: Theme.palette.primary.contrastText
               },
               border: 'none'
             }}
-          ><SearchIcon></SearchIcon>Buscar</Button>
+          >
+            <SearchIcon></SearchIcon>
+            Buscar
+          </Button>
         </Stack>
 
       </Stack>
