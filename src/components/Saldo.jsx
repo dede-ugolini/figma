f93fe6e14539a24bba56f88397da075fbc1b99a2
Andre Ugolini <andreugolini@unochapeco.edu.br>
2026-03-03 17:05:03 -0300

    Corrigir cores do saldo.

--- src/components/Saldo.jsx
+++ src/components/Saldo.jsx
@@ -14,14 +14,14 @@ export default function Saldo({ entradas, saidas, total }) {
 
   return (
     <>
-      <Stack direction={"row"} spacing={4} sx={{ width: "100%" }}>
-        <Card sx={{ /* backgroundColor: Theme.palette.secondary.grayThree, */ /* color: Theme.palette.text.title,  */width: "33%" }}>
+      <Stack direction={"row"} spacing={4} sx={{ width: "100%", height: "12vh" }}>
+        <Card sx={{ width: "33%" }}>
           <CardContent sx={{ paddingLeft: "10%", paddingTop: "10%", paddingBottom: "0px" }}>
             <Stack direction={"row"} sx={{ justifyContent: "space-between" }}>
               <Typography variant="h6">Entradas</Typography>
               <Typography variant="h6"> <ArrowCircleUpIcon color="primary" /> </Typography>
             </Stack>
-            <Typography variant="h5"> R$ {entradas.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</Typography>
+            <Typography variant="h5" fontWeight={"bold"}> R$ {entradas.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</Typography>
           </CardContent>
           <CardActions>
           </CardActions>
@@ -33,19 +33,19 @@ export default function Saldo({ entradas, saidas, total }) {
               <Typography variant="h6">Saídas</Typography>
               <Typography variant="h6"> <ArrowCircleDownIcon sx={{ color: '#F75A68' }} /> </Typography>
             </Stack>
-            <Typography variant="h5"> R$ {saidas.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</Typography>
+            <Typography variant="h5" fontWeight={"bold"}> R$ {saidas.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</Typography>
           </CardContent>
           <CardActions>
           </CardActions>
         </Card>
 
-        <Card sx={{/*  backgroundColor: Theme.palette.primary.dark, color: Theme.palette.text.title,  */width: "33%" }}>
+        <Card sx={(theme) => ({ background: theme.palette.mode === "dark" ? theme.palette.primary.dark : theme.palette.primary.main, width: "33%" })}>
           <CardContent sx={{ paddingLeft: "10%", paddingTop: "10%", paddingBottom: "0px" }}>
             <Stack direction={"row"} sx={{ justifyContent: "space-between" }}>
-              <Typography variant="h6">Total</Typography>
-              <Typography variant="h6"> <AttachMoneyIcon /> </Typography>
+              <Typography variant="h6" sx={{ color: "white" }}>Total</Typography>
+              <Typography variant="h6" sx={{ color: "white" }}> <AttachMoneyIcon /> </Typography>
             </Stack>
-            <Typography variant="h5"> R$ {total.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })} </Typography>
+            <Typography variant="h5" fontWeight={"bold"} sx={{ color: "white" }}> R$ {total.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })} </Typography>
           </CardContent>
           <CardActions>
           </CardActions>
